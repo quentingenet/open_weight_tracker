fb531f6b9bc91514424ec95266fcac456ad43863
Quentin Genet <genetquentin@gmail.com>
2024-01-27 11:22:18 +0100

    ♻️ Front: refactor Userservice

--- owt_front/src/services/UserService.ts
+++ owt_front/src/services/UserService.ts
@@ -3,51 +3,47 @@ import { ILoginForm } from '../models/ILoginForm';
 import { IRegisterForm } from '../models/IRegisterForm';
 import { API_URL } from '../utils/GlobalUtils';
 
-export const login = (data: ILoginForm) => {
+export const login = async (data: ILoginForm) => {
     const requestDataLogin = {
         username: data.username,
         password: data.password,
     };
-    return new Promise((resolve, reject) => {
-        fetch(API_URL.concat('token/'), {
+
+    try {
+        const response = await fetch(API_URL.concat('token/'), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(requestDataLogin),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    const headers = response.headers;
-                    const jwt = headers.get('Authorization') || '';
-                    if (localStorage.getItem('jwt')) {
-                        localStorage.removeItem('jwt');
-                    }
-                    localStorage.setItem('jwt', JSON.stringify(jwt));
-                    resolve(response.json());
-                } else {
-                    throw new Error("Erreur lors de la requête à l'API");
-                }
-            })
-            .catch((error) => {
-                reject(new Error("Erreur lors de l'appel à l'API :" + error));
-            });
-    });
+        });
+
+        if (response.ok) {
+            const jwt = response.headers.get('Authorization') || '';
+
+            if (localStorage.getItem('jwt')) {
+                localStorage.removeItem('jwt');
+            }
+
+            localStorage.setItem('jwt', JSON.stringify(jwt));
+            return response.json();
+        } else {
+            throw new Error("Erreur lors de la requête à l'API");
+        }
+    } catch (error) {
+        throw new Error("Erreur lors de l'appel à l'API :" + error);
+    }
 };
 
-// Assuming IRegisterForm interface is defined somewhere
-
 export const register = async (dataRegister: IRegisterForm) => {
-    // Creating an object with required data for registration
-    const requestDataRegister = {
-        username: dataRegister.username,
-        email: dataRegister.email,
-        password: dataRegister.password,
-        is_accepted_terms: dataRegister.isAcceptedTerms,
-    };
-
-    // Using async/await for cleaner asynchronous code
     try {
+        const requestDataRegister = {
+            username: dataRegister.username,
+            email: dataRegister.email,
+            password: dataRegister.password,
+            is_accepted_terms: dataRegister.isAcceptedTerms,
+        };
+
         const response = await fetch(API_URL.concat('register/'), {
             method: 'POST',
             headers: {
@@ -57,106 +53,82 @@ export const register = async (dataRegister: IRegisterForm) => {
         });
 
         if (response.ok) {
-            const headers = response.headers;
+            const jwt = response.headers.get('Authorization') || '';
 
-            // Extracting JWT token from headers
-            const jwt = headers.get('Authorization') || '';
-
-            // Clearing old JWT from local storage
             if (localStorage.getItem('jwt')) {
                 localStorage.removeItem('jwt');
             }
 
-            // Storing the new JWT in local storage
             localStorage.setItem('jwt', JSON.stringify(jwt));
 
-            // Parsing and returning the JSON response
             return response.json();
         } else {
             throw new Error('Error during API request');
         }
     } catch (error) {
-        throw new Error('Error calling API: ' + error);
+        throw new Error('Error during API request : ' + error);
     }
 };
 
-export const initData = (dataInitial: IInitialData) => {
-    const requestDataInitial = {
-        birthdate: dataInitial.birthdate,
-        gender: dataInitial.gender,
-        is_european_unit_measure: dataInitial.isEuropeanUnitMeasure,
-        body_size: dataInitial.bodySize,
-        goal_weight: dataInitial.goalWeight,
-        initial_weight: dataInitial.initialWeight,
-    };
+export const initData = async (dataInitial: IInitialData) => {
+    try {
+        const requestDataInitial = {
+            birthdate: dataInitial.birthdate,
+            gender: dataInitial.gender,
+            is_european_unit_measure: dataInitial.isEuropeanUnitMeasure,
+            body_size: dataInitial.bodySize,
+            goal_weight: dataInitial.goalWeight,
+            initial_weight: dataInitial.initialWeight,
+        };
 
-    return new Promise((resolve, reject) => {
         const myHeaders = new Headers();
         myHeaders.append('Content-Type', 'application/json');
         myHeaders.append('Authorization', ` ${localStorage.getItem('jwt')}`);
 
-        fetch(API_URL.concat('init/first-connexion/'), {
+        const response = await fetch(API_URL.concat('init/first-connexion/'), {
             method: 'POST',
             headers: myHeaders,
             body: JSON.stringify(requestDataInitial),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    console.log(response);
-                    if (response.status === 204) {
-                        resolve(); // Pas de contenu dans la réponse
-                    } else {
-                        response
-                            .text()
-                            .then((text) => {
-                                if (text.trim() === '') {
-                                    // Traitez le cas d'une réponse vide comme nécessaire
-                                    console.log(
-                                        "La réponse est vide, mais le statut n'est pas 204."
-                                    );
-                                    resolve(); // Ou faites quelque chose d'autre en conséquence
-                                } else {
-                                    // Essayez de parser le texte en JSON
-                                    try {
-                                        resolve(JSON.parse(text));
-                                    } catch (parseError) {
-                                        reject({
-                                            status: response.status,
-                                            message:
-                                                'Erreur de parsing JSON : ' +
-                                                parseError,
-                                        });
-                                    }
-                                }
-                            })
-                            .catch((error) => {
-                                reject({
-                                    status: response.status,
-                                    message:
-                                        'Erreur lors de la gestion de la réponse JSON : ' +
-                                        error.message,
-                                });
-                            });
-                    }
+        });
+
+        if (response.ok) {
+            console.log(response);
+
+            if (response.status === 204) {
+                return Promise.resolve(); // Pas de contenu dans la réponse
+            } else {
+                const text = await response.text();
+
+                if (text.trim() === '') {
+                    console.log(
+                        "La réponse est vide, mais le statut n'est pas 204."
+                    );
+                    return Promise.resolve();
                 } else {
-                    response.json().then((errorData) => {
-                        reject({
-                            status: response.status,
-                            message:
-                                errorData.message ||
-                                "Erreur lors de la requête à l'API",
-                        });
-                    });
+                    try {
+                        return Promise.resolve(JSON.parse(text));
+                    } catch (parseError) {
+                        throw {
+                            status: 500,
+                            message: 'Erreur de parsing JSON : ' + parseError,
+                        };
+                    }
                 }
-            })
-            .catch((error) => {
-                reject({
-                    status: 500,
-                    message:
-                        "Erreur lors de l'appel à l'API : " + error.message,
-                });
-            });
-    });
+            }
+        } else {
+            const errorData = await response.json();
+            throw {
+                status: response.status,
+                message:
+                    errorData.message || "Erreur lors de la requête à l'API",
+            };
+        }
+    } catch (error) {
+        throw {
+            status: 500,
+            message: "Erreur lors de l'appel à l'API : " + error,
+        };
+    }
 };
 
 export const resetAndChangePassword = (emailResetPassword: string) => {
