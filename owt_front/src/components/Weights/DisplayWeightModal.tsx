fe0cc1fc0839ab4e3bf6c9c2675ab5aa397648b8
Quentin Genet <genetquentin@gmail.com>
2024-02-11 20:05:16 +0100

    Styl displaying weights modal

--- owt_front/src/components/Weights/DisplayWeightModal.tsx
+++ owt_front/src/components/Weights/DisplayWeightModal.tsx
@@ -12,9 +12,10 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: { xs: '80%', sm: '70%', md: '40%', lg: '30%'},
-    bgcolor: '#384454',
-    border: '2px solid #000',
+    width: { xs: '50%', sm: '40%', md: '40%', lg: '30%' },
+    backgroundImage:
+        'linear-gradient(to top, #1e4164, #1b4e7e, #185b9a, #1669b5, #1976d2)',
+    border: '1px solid #000',
     boxShadow: 24,
     p: 4,
     color: 'white',
@@ -40,6 +41,7 @@ export default function DisplayWeightModal(props: any) {
                         id='modal-modal-title'
                         variant='h6'
                         component='h2'
+                        fontFamily={'Khand'}
                     >
                         <strong>
                             {dayjs(weightData.date).format(
@@ -47,23 +49,47 @@ export default function DisplayWeightModal(props: any) {
                             )}
                         </strong>
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>Weight:</strong> {weightData.weightValue} kg
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>Muscular mass:</strong>{' '}
                         {weightData.muscularMass} %
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>Fat mass:</strong> {weightData.fatMass} %
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>Bone mass:</strong> {weightData.boneMass} %
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>Water mass:</strong> {weightData.waterMass} %
                     </Typography>
-                    <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                    <Typography
+                        id='modal-modal-description'
+                        sx={{ mt: 2 }}
+                        fontFamily={'Khand'}
+                    >
                         <strong>BMI:</strong> {weightData.bmi}
                     </Typography>
                 </Box>
