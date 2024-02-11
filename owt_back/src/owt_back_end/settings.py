4b7b2b4eaccd446eae3b788bdc1ed2093bcfba22
Quentin Genet <genetquentin@gmail.com>
2024-02-11 19:51:39 +0100

    env variable for debug

--- owt_back/src/owt_back_end/settings.py
+++ owt_back/src/owt_back_end/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = decouple.config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = decouple.config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["kent121.eu.pythonanywhere.com","localhost"]
 
